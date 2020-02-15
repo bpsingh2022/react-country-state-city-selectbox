cf55d518347359b99ce1d4ffc11075aee1aa6ccd
bpsingh2022 <bpsingh01012022@gmail.com>
2020-02-15 15:34:56 +0530

    'update'

--- src/index.js
+++ src/index.js
@@ -1,78 +1,45 @@
-Ahmedabad
-Gujarat
-511
-Amreli
-Gujarat
-350
-Anand
-Gujarat
-263
-Banaskantha
-Gujarat
-405
-Bharuch
-Gujarat
-330
-Bhavnagar
-Gujarat
-405
-Dahod
-Gujarat
-228
-Gandhi Nagar
-Gujarat
-197
-Jamnagar
-Gujarat
-387
-Junagadh
-Gujarat
-448
-Kachchh
-Gujarat
-491
-Kheda
-Gujarat
-334
-Mahesana
-Gujarat
-315
-Narmada
-Gujarat
-186
-Navsari
-Gujarat
-288
-Panch Mahals
-Gujarat
-342
-Patan
-Gujarat
-263
-Porbandar
-Gujarat
-98
-Rajkot
-Gujarat
-492
-Sabarkantha
-Gujarat
-576
-Surat
-Gujarat
-639
-Surendra Nagar
-Gujarat
-330
-Tapi
-Gujarat
-11
-The Dangs
-Gujarat
-45
-Vadodara
-Gujarat
-651
-Valsad
-Gujarat
-268
+import React from 'react';
+
+export const city = [
+    {id: 1, name: "Ahmedabad", state: "gujarat", country: "india" },
+{id: 2, name: "Amreli", state: "gujarat", country: "india" },
+{id: 3, name: "Anand", state: "gujarat", country: "india" },
+{id: 4, name: "Banaskantha", state: "gujarat", country: "india" },
+{id: 5, name: "Bharuch", state: "gujarat", country: "india" },
+{id: 6, name: "Bhavnagar", state: "gujarat", country: "india" },
+{id: 7, name: "Dahod", state: "gujarat", country: "india" },
+{id: 8, name: "Gandhi Nagar", state: "gujarat", country: "india" },
+{id: 9, name: "Jamnagar", state: "gujarat", country: "india" },
+{id: 10, name: "Junagadh", state: "gujarat", country: "india" },
+{id: 11, name: "Kachchh", state: "gujarat", country: "india" },
+{id: 12, name: "Kheda", state: "gujarat", country: "india" },
+{id: 13, name: "Mahesana", state: "gujarat", country: "india" },
+{id: 14, name: "Narmada", state: "gujarat", country: "india" },
+{id: 15, name: "Navsari", state: "gujarat", country: "india" },
+{id: 16, name: "Panch Mahals", state: "gujarat", country: "india" },
+{id: 17, name: "Patan", state: "gujarat", country: "india" },
+{id: 18, name: "Porbandar", state: "gujarat", country: "india" },
+{id: 19, name: "Rajkot", state: "gujarat", country: "india" },
+{id: 20, name: "Sabarkantha", state: "gujarat", country: "india" },
+{id:21, name: "Surat", state: "gujarat", country: "india" },
+{id: 22, name: "Surendra Nagar", state: "gujarat", country: "india" },
+{id: 23, name: "Tapi", state: "gujarat", country: "india" },
+{id: 24, name: "The Dangs", state: "gujarat", country: "india" },
+{id: 25, name: "Vadodara", state: "gujarat", country: "india" },
+{id: 26, name: "Valsad", state: "gujarat", country: "india" },
+];//endCityArr;
+
+export const state = [
+    {id:1, name: "gujarat", country: "india"}
+];//endState;
+export const country = [
+    {id:1, name: "india", countryCode: 91}
+];//endCountry;
+
+export default ()=>{
+
+return(
+<>
+<div>test for bobby</div>
+</>
+)};//end;
